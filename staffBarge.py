7aae58116d1b4e8964b6675a5c33a50233625b22
dungasamsungo <sampsonngobi@gmail.com>
2023-11-05 23:15:56 -0300

    added two more lines and did some formatting

--- staffBarge.py
+++ staffBarge.py
@@ -6,8 +6,15 @@ email = input("Email address: ").lower()
 phone_number = input("Phone number: ")
 job_title = input("Job Title: ").capitalize()
 id = input("Id: ")
+hair = input("Hair color: ").capitalize()
+eyes = input("Eyes color: ").capitalize()
+month = input("Month: ").capitalize()
+status = input("Traning (Yes/No): ").capitalize()
+
+
 
 print("\nThe ID Card is:")
 print("-------------------------")
 print(f"{last_name}, {first_name}\n{job_title}\n{id}\n\n{email}\n{phone_number}")
-print("-------------------------")
+print(f"Hair: {hair:15} Eyes: {eyes}\nMonht: {month:14} Training: {status} ")
+print("-------------------------\n")
